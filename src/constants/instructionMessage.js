6042359f6052be33c795b83679729174b7822b60
Fatima Alam <71816449+alamfatima1999@users.noreply.github.com>
2024-07-02 23:05:12 +0530

    Update instructionMessage.js

--- src/constants/instructionMessage.js
+++ src/constants/instructionMessage.js
@@ -3,4 +3,4 @@ export const InstructionMessage2 = `Drag 'n' Drop Sprite/Action to the left to R
 export const InstructionMessage3 = `Drag 'n' Drop Action to the mid area`;
 export const InstructionMessage4 = `Press Run to execute current sprite actions, Press Run All to execute all sprite actions, & Reset to reset cat position`;
 export const InstructionMessage5 = `Add a number & you can replay last n actions performed on the sprite`;
-export const InstructionMessage5 = `You can use the arrow keys to move the sprite. Just make sure to click on or select the sprite container before using the arrow keys.`;
+export const InstructionMessage6 = `You can use the arrow keys to move the sprite. Just make sure to click on or select the sprite container before using the arrow keys.`;
